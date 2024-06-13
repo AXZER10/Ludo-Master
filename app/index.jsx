cd0f834f3c68ea3fd38adb7b09e6c2f149b97287
Pranjal Snehi <pranjal.snehi@gmail.com>
2024-06-13 14:53:11 +0530

    Custom button added and fonts added

--- app/index.jsx
+++ app/index.jsx
@@ -1,19 +1,33 @@
-import { SafeAreaView, Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Image, SafeAreaView, Text, View } from "react-native";
+import { Link, router } from "expo-router";
+import CustomButton from "../components/CustomButton";
 
 export default function Index() {
   return (
     <SafeAreaView className='h-full bg-black'>
-      <View className="flex-1 justify-center items-center"
-      /*style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}*/
-    >
+      <View className=' items-center w-full px-4'>
+        <Image source={require('./assets/Logo.jpeg')} 
+        className='w-40 h-40 justify-center'
+        resizeMode="contain"
+        />
+        <Text className="text-white text-3xl font-psemibold">
+          Ludo Master
+        </Text>
+      </View>
+    <View className="flex-column mt-20 items-center px-4 justify-center">
       <Link href='/Ludo'>
       <Text className="text-3xl text-white">Go To Game</Text>
       </Link>
+      <View>
+
+      </View>
+      <View className=' w-full my-8 items-center'>
+        <CustomButton 
+        title={'Play'} 
+        ContainerStyles={'w-40'}
+        handlePress={() => router.push("/Ludo")}
+        />
+      </View>
     </View>
     </SafeAreaView>
   );
---
+++ components/CustomButton.jsx
@@ -0,0 +1,19 @@
+// CustomButton.js
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const CustomButton = ({ title, handlePress,ContainerStyles, textStyles, isLoading }) => {
+  return (
+    <TouchableOpacity 
+      onPress={handlePress}
+      activeOpacity={0.6}
+      className={` bg-green-300 rounded-[60px] min-h-[62px] justify-center items-center ${ContainerStyles} ${isLoading ? 'opacity-50' : ''}`} 
+      disabled={isLoading} 
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}'`} >{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+
+export default CustomButton;
