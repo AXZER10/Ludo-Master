b24ca0bc1199774cb95fee0d61805fa1dc0d2f93
Nikky Pal <nikhilpal1000@gmail.com>
2024-06-17 15:25:40 +0530

    add file friends

--- app/(tabs)/friend.jsx
+++ app/(tabs)/friend.jsx
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const friend = () => {
   return (
-    <SafeAreaView>
-    <View className ="px-4">
-      <Text>Friend</Text>
+    <SafeAreaView className="  h-full justify-center items-center bg-primary">
+    <View className="flex-column justify-center space-y-2 items-center">
+      <Text className="items-center font-psemibold justify-center text-2xl text-blue-400">
+        CHALLENGES
+      </Text>
+      <Text className="items-center font-psemibold justify-center text-2xl text-blue-400">
+        Buddy  CHALLENGES
+      </Text>
+      <Text className="items-center font-psemibold justify-center text-2xl text-blue-400">
+       Facebook friends
+      </Text>
     </View>
     </SafeAreaView>
   )
--- constants/icons.js
+++ constants/icons.js
@@ -1,7 +1,7 @@
 import home from "../app/assets/icons/home.png";
 import menu from "../app/assets/icons/menu.png";
 import profile from "../app/assets/icons/profile.png";
-import friend from "../app/assets/icons/fri2.png";
+import friend from "../app/assets/icons/friend.png"
 
 
 export default{
