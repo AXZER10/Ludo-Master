a2a47298d1c8efa3c51062fbaa3caf4d5d7debbe
Pranjal Snehi <pranjal.snehi@gmail.com>
2024-07-04 16:28:29 +0530

    Added create and join room functionality

--- app/Room.jsx
+++ app/Room.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Alert, View, StyleSheet } from 'react-native';
-import { getFirestore, collection, addDoc, query, where, getDocs, updateDoc, doc, getDoc } from 'firebase/firestore';
+import { getFirestore, collection, addDoc, query, where, getDocs, updateDoc, doc, onSnapshot } from 'firebase/firestore';
 import { auth } from '../FirebaseConfig';
 import CustomButton from "../components/CustomButton";
+import { useRouter } from 'expo-router';
 
 const LudoTwoPlayer = () => {
   const [roomId, setRoomId] = useState('');
   const [playerUid, setPlayerUid] = useState('');
-  const [gameStarted, setGameStarted] = useState(false);
-  const [users, setPlayers] = useState([]);
+  const router = useRouter();
 
-  useEffect(() => {            
+  useEffect(() => {
     const user = auth.currentUser;
     if (user) {
       setPlayerUid(user.uid);
@@ -51,6 +51,11 @@ const LudoTwoPlayer = () => {
       });
       setRoomId(createRef.id); // Store the created room ID
       Alert.alert('Room Created', `Room ID: ${createRef.id}`);
+      
+      setTimeout(() => {
+        startGame(createRef.id);
+      }, 1000);
+
     } catch (error) {
       Alert.alert('Error', 'Could not create room. Please try again.');
     }
@@ -61,21 +66,42 @@ const LudoTwoPlayer = () => {
     try {
       const roomRef = doc(db, 'twoPlayerRooms', roomId);
       await updateDoc(roomRef, {
-        uid2: uid
+        uid2: uid,
+        gameState: "Started"
       });
-      const updatedDoc = await getDoc(roomRef);
-      const { uid1, uid2, gameStarted, players } = updatedDoc.data();
-      setRoomId(roomId);
-      setPlayers([{ uid1 }, { uid2 }]);
-      setGameStarted(gameStarted);
+
+      startGame(roomId);
     } catch (error) {
       Alert.alert('Error joining room:', error);
     }
   };
 
+  const startGame = (roomId) => {
+    const db = getFirestore();
+    const roomRef = doc(db, 'twoPlayerRooms', roomId);
+    const unsubscribe = onSnapshot(roomRef, (doc) => {
+      if (doc.exists()) {
+        const gameState = doc.data().gameState;
+        if (gameState === "Started") {
+          console.log('Match found. Starting in 5 seconds');
+          setTimeout(() => {
+            router.push('/LudoTwoPlayer');
+          }, 5000);
+        }
+      } else {
+        Alert.alert('Document not found');
+      }
+    }, (error) => {
+      Alert.alert("Error starting game", error.message);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  };
+
   return (
     <View className="flex-col w-40 mx-2">
-      
+       
     </View>
   );
 };
