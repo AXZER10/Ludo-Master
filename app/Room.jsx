212de39808a74b02d7d9bd6a10dd3aa09201fd71
Pranjal Snehi <pranjal.snehi@gmail.com>
2024-07-05 11:42:36 +0530

    Added back button and game cancelled when going back

--- app/Room.jsx
+++ app/Room.jsx
@@ -6,7 +6,7 @@ import { router } from 'expo-router';
 import icons from '../constants/icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const LudoTwoPlayer = () => {
+const Room = () => {
   const [roomId, setRoomId] = useState('');
   const [playerUid, setPlayerUid] = useState('');
   const [countdown, setCountdown] = useState(null);
@@ -34,8 +34,9 @@ const LudoTwoPlayer = () => {
     try {
       const q = query(roomRef, where('uid2', '==', ''), where('uid1', '!=', uid));
       const querySnapshot = await getDocs(q);
-      if (!querySnapshot.empty) {
-        const roomDoc = querySnapshot.docs[0];
+      const filteredRooms = querySnapshot.docs.filter(doc => doc.data().uid1 !== uid && doc.data().uid1 !== '');
+      if (filteredRooms.length > 0) {
+        const roomDoc = filteredRooms[0];
         const roomId = roomDoc.id;
         await joinRoom(roomId, uid);
       } else {
@@ -116,19 +117,38 @@ const LudoTwoPlayer = () => {
     
   };
 
+  const Cancel = async() => {
+    const db = getFirestore();
+    try {
+      const roomRef = doc(db, 'twoPlayerRooms', roomId);
+      await updateDoc(roomRef, {
+        uid1: '',
+        uid2: '',
+        gameState: "Cancelled"
+      });
+
+      router.replace('/Home')
+    } catch (error) {
+      Alert.alert('Error exiting room:', error.message);
+    }
+  }
+
   return (
     <SafeAreaView className="bg-primary h-full">
-      <View className="flex-col w-10 h-10 mx-2">
-       <TouchableOpacity
-         activeOpacity={0.7}
-         //onPress={router.replace('/Home')}
-       >
-        <Image source={icons.back}
-          resizeMode='contain'
-          className="w-10 h-10"
-        />
-       </TouchableOpacity>
-      </View>
+      
+      {countdown == null && (
+        <View className="flex-col w-10 h-10 mx-2">
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => Cancel()}
+        >
+         <Image source={icons.back}
+           resizeMode='contain'
+           className="w-10 h-10"
+         />
+        </TouchableOpacity>
+       </View>
+      )}
       {countdown !== null && (
         <View className="flex-col w-full h-full justify-center items-center">
           <Text className="text-5xl font-pbold text-slate-600">Game starts in {countdown}...</Text>
@@ -138,4 +158,4 @@ const LudoTwoPlayer = () => {
   );
 };
 
-export default LudoTwoPlayer;
+export default Room;
