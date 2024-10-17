9366a035bebd44de10fe9616cc671dc1f2464539
Pranjal Snehi <pranjal.snehi@gmail.com>
2024-10-17 10:49:40 +0530

    minor Changes in ui

--- app/(tabs)/wallet.jsx
+++ app/(tabs)/wallet.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, Image, ImageBackground,FlatList } from "react-native";
+import { View, Text, TouchableOpacity, Image, ImageBackground, FlatList } from "react-native";
 import { useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { getFirestore } from "firebase/firestore";
@@ -10,7 +10,7 @@ import TopBar from "../../components/TopBar";
 const WalletScreen = () => {
   const [totalBalance] = useState();
   const [winnings] = useState();
-  const router= useRouter();
+  const router = useRouter();
   const user = auth().currentUser;
 
   const db = getFirestore
@@ -19,7 +19,7 @@ const WalletScreen = () => {
       try {
         const uid = user.uid; // Replace with dynamic user ID
         const walletDoc = await firestore().collection('wallet').doc(uid).get();
-        
+
         if (walletDoc.exists) {
           const data = walletDoc.data();
           setTotalBalance(data.totalBalance);
@@ -35,23 +35,24 @@ const WalletScreen = () => {
     fetchWalletData();
   }, []);
   return (
-    <SafeAreaView className="h-full w-full justify-center items-center ">
-      <ImageBackground source={require("../assets/bg.png")}
-                  resizeMode='cover'
-                  className="h-full w-full "
-                  >
-                    <FlatList 
-ListHeaderComponent={() => (
-  <TopBar/>
-)}
-renderItem={null}
-ListFooterComponent={() => (
-  <>
-      <View className="flex-row justify-between ml-15">
-        <Text className="text-white font-psemibold text-xl">
-          Wallet
-        </Text>
-        {/* <TouchableOpacity className="px-8 ml-32" >
+    <ImageBackground source={require("../assets/bg.png")}
+      resizeMode='cover'
+      className="h-full w-full"
+    >
+      <SafeAreaView>
+        <TopBar />
+        <FlatList
+          ListHeaderComponent={() => (
+            <></>
+          )}
+          renderItem={null}
+          ListFooterComponent={() => (
+            <View className="p-5">
+              <View className="flex-row justify-between ml-15">
+                <Text className="text-white font-psemibold text-xl">
+                  Wallet
+                </Text>
+                {/* <TouchableOpacity className="px-8 ml-32" >
           <Image className="w-10 h-10"
             source={require("../assets/headphone.png")}
           />
@@ -61,59 +62,58 @@ ListFooterComponent={() => (
             source={require("../assets/Settings-L-icon.png")}
           />
         </TouchableOpacity> */}
-      </View>
+              </View>
+              <View className="px-2 items-center mr-40">
+                <Text className="text-white font-psemibold text-xl">
+                  Total Balance
+                </Text>
+                <Text className="text-white font-psemibold text-xl">
+                  ₹{totalBalance}
+                </Text>
+              </View>
 
-      <View className="px-2 items-center mr-40">
-        <Text className="text-white font-psemibold text-xl">
-          Total Balance
-        </Text>
-        <Text className="text-white font-psemibold text-xl">
-          ₹{totalBalance}
-         </Text>
-          </View>
+              <View className="flex-row justify-between p-8">
+                <TouchableOpacity className="flex-1 bg-black rounded-3xl p-2"
+                  onPress={() => router.push('deposit')}
+                >
+                  <Text className="text-white text-center font-psemibold text-xl">
+                    Deposit
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity className="flex-row bg-black rounded-3xl p-2"
+                  onPress={() => router.push('transaction')}>
+                  <Text className="text-white text-center font-psemibold text-xl">
+                    Transactions
+                  </Text>
+                </TouchableOpacity>
+              </View>
 
-      <View className="flex-row justify-between p-8">
-        <TouchableOpacity className="flex-1 bg-black rounded-3xl p-2"
-        onPress={ () => router.push('deposit')}
-        >
-          <Text className="text-white text-center font-psemibold text-xl">
-            Deposit
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="flex-row bg-black rounded-3xl p-2"
-        onPress={() => router.push('transaction')}>
-          <Text className="text-white text-center font-psemibold text-xl">
-            Transactions
-          </Text>
-        </TouchableOpacity>
-      </View>
+              <View className="px-2">
+                <Text className="text-white font-psemibold text-lg">
+                  Winnings
+                </Text>
+                <Text className="text-white font-psemibold text-xl">
+                  ₹{winnings}
+                </Text>
+              </View>
 
-      <View  className="px-2">
-        <Text className="text-white font-psemibold text-lg">
-          Winnings
-        </Text>
-        <Text className="text-white font-psemibold text-xl">
-          ₹{winnings}
-        </Text>
-      </View>
+              <View className="flex-row justify-between mt-8">
+                <TouchableOpacity className="flex-1 bg-black rounded-3xl p-3"
+                  onPress={() => router.push('withdraw')}>
+                  <Text className="text-white text-center font-psemibold text-xl">
+                    Withdraw
+                  </Text>
+                </TouchableOpacity>
+              </View>
 
-      <View className="flex-row justify-between mt-8">
-        <TouchableOpacity className="flex-1 bg-black rounded-3xl p-3"
-        onPress={() => router.push('withdraw')}>
-          <Text className="text-white text-center font-psemibold text-xl">
-            Withdraw
-          </Text>
-        </TouchableOpacity>
-      </View>
-
-      <View className=" p-2 items-center">
-        <Text className="text-white text-sm">100% Money Safety</Text>
-      </View>
-      </>
-)}
-/>
-      </ImageBackground>
-    </SafeAreaView>
+              <View className=" p-2 items-center">
+                <Text className="text-white text-sm">100% Money Safety</Text>
+              </View>
+            </View>
+          )}
+        />
+      </SafeAreaView >
+    </ImageBackground>
   );
 };
 
